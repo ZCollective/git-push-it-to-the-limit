d5edaebce89bb59d5d9b2dee874857c96f5f31ab
Michael Zigldrum <michael.zigldrum@veoneer.com>
2019-12-29 21:41:28 +0000

    Initial Commit

---
+++ playsound.py
@@ -0,0 +1,18 @@
+from playsoundlib import playsound
+import random
+songs = [
+  './sound/push-it-short.mp3',
+  './sound/quavo-short.mp3',
+  './sound/rick-roll-short.mp3',
+  './sound/scarface-short.mp3',
+  './sound/sleigh-bells-short.mp3',
+  './sound/triple-master-blaster-short.mp3'
+]
+
+playsound(random.choice(songs))
+# Quavo Push It
+# saltn pepa push it
+# salt sled true shred guitar
+# Smokey Robinson & the Miracles love machine
+#Crash test dummies triple master blaster
+# rick astley never gonna give you up
